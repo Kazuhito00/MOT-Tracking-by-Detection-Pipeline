7e70a14cf12bccbd9c42e831c3de8c54f24ac6d7
KazuhitoTakahashi <shigeki.takahashi.dev@gmail.com>
2022-02-16 22:54:32 +0900

    --use_gpuオプション対応。

--- Detector/detector.py
+++ Detector/detector.py
@@ -1,19 +1,29 @@
 import copy
 import json
 
+import numpy as np
+
 
 class ObjectDetector(object):
     def __init__(
         self,
         model_name='yolox',
-        providers=['CPUExecutionProvider'],
+        target_id=None,
+        use_gpu=False,
     ):
         self.model_name = model_name
         self.model = None
         self.config = None
+        self.target_id = target_id
+        self.use_gpu = use_gpu
 
         if self.model_name == 'yolox':
             from Detector.yolox.yolox_onnx import YoloxONNX
+
+            if self.use_gpu:
+                providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
+            else:
+                providers = ['CPUExecutionProvider']
 
             with open('Detector/yolox/config.json') as fp:
                 self.config = json.load(fp)
@@ -34,6 +44,11 @@ class ObjectDetector(object):
         elif self.model_name == 'efficientdet':
             from Detector.efficientdet.efficientdet_onnx import EfficientDetONNX
 
+            if self.use_gpu:
+                providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
+            else:
+                providers = ['CPUExecutionProvider']
+
             with open('Detector/efficientdet/config.json') as fp:
                 self.config = json.load(fp)
 
@@ -49,6 +64,11 @@ class ObjectDetector(object):
 
         elif self.model_name == 'ssd':
             from Detector.ssd.ssd_onnx import SsdONNX
+
+            if self.use_gpu:
+                providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
+            else:
+                providers = ['CPUExecutionProvider']
 
             with open('Detector/ssd/config.json') as fp:
                 self.config = json.load(fp)
@@ -66,6 +86,11 @@ class ObjectDetector(object):
         elif self.model_name == 'centernet':
             from Detector.centernet.centernet_onnx import CenterNetONNX
 
+            if self.use_gpu:
+                providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
+            else:
+                providers = ['CPUExecutionProvider']
+
             with open('Detector/centernet/config.json') as fp:
                 self.config = json.load(fp)
 
@@ -81,6 +106,11 @@ class ObjectDetector(object):
 
         elif self.model_name == 'nanodet':
             from Detector.nanodet.nanodet_onnx import NanoDetONNX
+
+            if self.use_gpu:
+                providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
+            else:
+                providers = ['CPUExecutionProvider']
 
             with open('Detector/nanodet/config.json') as fp:
                 self.config = json.load(fp)
@@ -99,6 +129,8 @@ class ObjectDetector(object):
         elif self.model_name == 'mediapipe_face':
             from Detector.mediapipe_face.mediapipe_face import MediaPipeFaceDetection
 
+            self.use_gpu = False  # GPU使用不可
+
             with open('Detector/mediapipe_face/config.json') as fp:
                 self.config = json.load(fp)
 
@@ -111,6 +143,8 @@ class ObjectDetector(object):
 
         elif self.model_name == 'mediapipe_hand':
             from Detector.mediapipe_hand.mediapipe_hand import MediaPipeHands
+
+            self.use_gpu = False  # GPU使用不可
 
             with open('Detector/mediapipe_hand/config.json') as fp:
                 self.config = json.load(fp)
@@ -133,10 +167,19 @@ class ObjectDetector(object):
         input_image = copy.deepcopy(image)
         bboxes, scores, class_ids = None, None, None
 
+        # 推論
         if self.model is not None:
             bboxes, scores, class_ids = self.model(input_image)
         else:
             raise ValueError('Model is None')
+
+        # 対象のクラスIDで抽出
+        if self.target_id is not None and len(bboxes) > 0:
+            target_index = np.in1d(class_ids, np.array(self.target_id))
+
+            bboxes = bboxes[target_index]
+            scores = scores[target_index]
+            class_ids = class_ids[target_index]
 
         return bboxes, scores, class_ids
 
@@ -144,5 +187,8 @@ class ObjectDetector(object):
         from pprint import pprint
 
         print('Detector:', self.model_name)
+        print('Target ID:',
+              'ALL' if self.target_id is None else self.target_id)
+        print('GPU:', self.use_gpu)
         pprint(self.config, indent=4)
         print()
